7859fa9d60fdb23be9b04c282bfea007699b172e
TyomoGit <riku3dno.1@gmail.com>
2024-03-24 01:51:03 +0900

    refactor

--- src/parser.rs
+++ src/parser.rs
@@ -3,8 +3,12 @@ use crate::{
     print::zeta_error,
 };
 
+const MESSAGE_TAG: &str = "message";
+const DETAILS_TAG: &str = "details";
+
 pub struct Parser {
     source: Vec<char>,
+
     position: usize,
     start: usize,
 
@@ -23,15 +27,14 @@ impl Parser {
     }
 
     pub fn parse_file(mut self) -> MarkdownFile {
-        self.consume_string("---\n");
+        self.expect_string("---\n");
         self.delete_buffer();
 
-        while self.peek() != Some('-') {
-            self.advance();
-        }
-        let header = self.get_buffer().unwrap();
+        self.extract_until('-');
+            
+        let header = self.consume_buffer().unwrap();
 
-        self.consume_string("---\n");
+        self.expect_string("---\n");
         self.delete_buffer();
 
         let Ok(header) = serde_yaml::from_str(header.as_str()) else {
@@ -48,7 +51,7 @@ impl Parser {
             self.parse_element();
         }
 
-        self.consume_buffer();
+        self.collect_text();
 
         self.result
     }
@@ -61,212 +64,15 @@ impl Parser {
 
         match c {
             '[' => {
-                self.consume_buffer();
-                self.consume_string("[");
-
-                while self.peek() != Some(')') && !self.is_at_end() {
-                    self.advance();
-                }
-
-                if !self.is_at_end() {
-                    self.advance();
-                }
+                self.square_brackets_or_link();
             }
             '<' => {
                 if !self.matches_keyword("<macro>") {
                     self.advance();
                     return;
                 }
-                self.consume_buffer();
-
-                while self.peek() != Some('\n') {
-                    self.advance();
-                }
-                self.consume_string("\n");
-                self.delete_buffer();
-                while self.peek() != Some('<') {
-                    self.advance();
-                }
-                let macro_yaml = self.get_buffer().unwrap();
-
-                self.consume_string("</macro>");
-                self.delete_buffer();
                 
-
-                let Ok(macro_yaml): Result<Macro, _> = serde_yaml::from_str(macro_yaml.as_str()) else {
-                    zeta_error("Invalid macro yaml");
-                    return;
-                };
-                
-
-                self.result.push(Element::Macro(macro_yaml));
-
-            }
-            'h' => {
-                if !(self.matches_keyword("https://") || self.matches_keyword("http://")) {
-                    self.advance();
-                    return;
-                }
-
-                self.consume_buffer();
-
-                while self.peek().is_some() && !self.peek().unwrap().is_whitespace() {
-                    self.advance();
-                }
-
-                let url = self.get_buffer().unwrap();
-                self.result.push(Element::Url(url));
-            }
-            '\n' => {
-                self.advance();
-                let Some(c_next) = self.peek() else {
-                    return;
-                };
-                match c_next {
-                    ':' => {
-                        const MESSAGE_TAG: &str = "message";
-                        const DETAILS_TAG: &str = "details";
-
-                        if self.matches_keyword(format!(":::{DETAILS_TAG}").as_str()) {
-                            self.consume_buffer();
-                            while matches!(self.peek(), Some(':')) {
-                                self.advance();
-                            }
-
-                            self.consume_string(DETAILS_TAG);
-
-                            self.advance_spaces();
-
-                            self.delete_buffer();
-                            while self.peek() != Some('\n') {
-                                self.advance();
-                            }
-                            let title = self.get_buffer().unwrap_or("".to_string());
-
-                            if let Some('\n') = self.peek() {
-                                self.advance();
-                            }
-                            self.delete_buffer();
-                            while self.peek() != Some(':') {
-                                self.advance();
-                            }
-                            let content = self.get_buffer().unwrap_or("".to_string());
-                            let parser = Parser::new(content.chars().collect());
-                            let content = parser.parse();
-                            self.result.push(Element::Details {
-                                title,
-                                body: content,
-                            });
-
-                            while matches!(self.peek(), Some(':')) {
-                                self.advance();
-                            }
-
-                            self.delete_buffer();
-
-                            return;
-                        }
-
-                        if !self.matches_keyword(format!(":::{MESSAGE_TAG}").as_str()) {
-                            return;
-                        }
-                        self.consume_buffer();
-
-                        while matches!(self.peek(), Some(':')) {
-                            self.advance();
-                        }
-
-                        (0..MESSAGE_TAG.len()).for_each(|_| {
-                            self.advance();
-                        });
-
-                        self.advance_spaces();
-
-                        let message_type = if self.matches_keyword("info") {
-                            MessageType::Info
-                        } else if self.matches_keyword("warn") {
-                            MessageType::Warn
-                        } else if self.matches_keyword("alert") {
-                            MessageType::Alert
-                        } else {
-                            zeta_error("Invalid message type");
-                            MessageType::Info
-                        };
-
-                        while self.peek() != Some('\n') {
-                            self.advance();
-                        }
-
-                        self.consume_string("\n");
-
-                        self.delete_buffer();
-
-                        while self.peek() != Some(':') {
-                            self.advance();
-                        }
-
-                        let content = self.get_buffer().unwrap();
-                        let parser = Parser::new(content.chars().collect());
-                        let content = parser.parse();
-                        self.result.push(Element::Message {
-                            msg_type: message_type,
-                            body: content,
-                        });
-
-                        while matches!(self.peek(), Some(':')) {
-                            self.advance();
-                        }
-
-                        self.delete_buffer();
-                    }
-
-                    '`' => {
-                        if self.peek_next() != Some('`') {
-                            // inline
-                            while self.peek() != Some('`') {
-                                self.advance();
-                            }
-                            self.consume_string("`");
-                        } else if self.matches_keyword("```") {
-                            // block
-                            self.consume_string("```");
-
-                            while self.peek() != Some('`') && !self.is_at_end() {
-                                self.advance();
-                            }
-
-                            self.consume_string("```");
-                        }
-                    }
-
-                    '!' => {
-                        if !self.matches_keyword("![") {
-                            return;
-                        }
-                        self.consume_buffer();
-                        self.consume_string("![");
-                        self.delete_buffer();
-
-                        while self.peek() != Some(']') {
-                            self.advance();
-                        }
-
-                        let alt = self.get_buffer().unwrap_or("".to_string());
-                        self.consume_string("](");
-                        self.delete_buffer();
-
-                        while self.peek() != Some(')') {
-                            self.advance();
-                        }
-
-                        let url = self.get_buffer().unwrap();
-                        self.consume_string(")");
-                        self.delete_buffer();
-
-                        self.result.push(Element::Image { alt, url });
-                    }
-                    _ => (),
-                }
+                self.macro_call();
             }
 
             '^' => {
@@ -274,20 +80,13 @@ impl Parser {
                     self.advance();
                     return;
                 }
-                self.consume_buffer();
+                 self.inline_footnote();
+            }
 
-                self.consume_string("^[");
-                self.delete_buffer();
-
-                while self.peek() != Some(']') {
-                    self.advance();
-                }
-
-                let inline_footnote = self.get_buffer().unwrap();
-                self.result.push(Element::InlineFootnote(inline_footnote));
-
-                self.consume_string("]");
-                self.delete_buffer();
+            '\n' => {
+                self.advance();
+                
+                self.parse_block_element();
             }
 
             _ => {
@@ -296,19 +95,220 @@ impl Parser {
         }
     }
 
+    fn parse_block_element(&mut self) {
+        let Some(c_next) = self.peek() else {
+            return;
+        };
+        
+        match c_next {
+            'h' => {
+                if !(self.matches_keyword("https://") || self.matches_keyword("http://")) {
+                    self.advance();
+                    return;
+                }
+
+                self.url();
+            }
+            ':' => {
+                if self.matches_keyword(format!(":::{DETAILS_TAG}").as_str()) {
+                    self.details();
+                } else if self.matches_keyword(format!(":::{MESSAGE_TAG}").as_str()) {
+                    self.message();
+                }
+            }
+
+            '`' => {
+                self.code_block();
+            }
+
+            '!' => {
+                if !self.matches_keyword("![") {
+                    return;
+                }
+                self.image();
+            }
+            _ => (),
+        }
+    }
+
+    fn square_brackets_or_link(&mut self) {
+        self.extract_until(']');
+        self.expect_string("]");
+        
+
+        if self.matches_keyword("(") {
+
+            self.extract_until(')');
+            self.expect_string(")");
+        }
+    }
+
+    fn macro_call(&mut self) {
+        self.collect_text();
+
+        self.extract_until('\n');
+        self.expect_string("\n");
+        self.delete_buffer();
+        self.extract_until('<');
+
+        let macro_yaml = self.consume_buffer().unwrap();
+
+        self.expect_string("</macro>");
+        self.delete_buffer();
+        
+
+        let Ok(macro_yaml): Result<Macro, _> = serde_yaml::from_str(macro_yaml.as_str()) else {
+            zeta_error("Invalid macro yaml");
+            return;
+        };
+        
+
+        self.result.push(Element::Macro(macro_yaml));
+    }
+
+    fn inline_footnote(&mut self) {
+        self.collect_text();
+
+        self.expect_string("^[");
+        self.delete_buffer();
+
+        self.extract_until(']');
+
+        let inline_footnote = self.consume_buffer().unwrap();
+        self.result.push(Element::InlineFootnote(inline_footnote));
+
+        self.expect_string("]");
+        self.delete_buffer();
+    }
+
+    fn url(&mut self) {
+        self.collect_text();
+        
+        while self.peek().is_some() && !self.peek().unwrap().is_whitespace() {
+            self.advance();
+        }
+
+        let url = self.consume_buffer().unwrap();
+        self.result.push(Element::Url(url));
+    }
+
+    fn details(&mut self) {
+        self.collect_text();
+        while matches!(self.peek(), Some(':')) {
+            self.advance();
+        }
+
+        self.expect_string(DETAILS_TAG);
+
+        self.advance_spaces();
+
+        self.delete_buffer();
+        self.extract_until(':');
+
+        let title = self.consume_buffer().unwrap_or_default();
+
+        if let Some('\n') = self.peek() {
+            self.advance();
+        }
+        self.delete_buffer();
+        self.extract_until(':');
+        let content = self.consume_buffer().unwrap_or_default();
+        let parser = Parser::new(content.chars().collect());
+        let content = parser.parse();
+        self.result.push(Element::Details {
+            title,
+            body: content,
+        });
+
+        while matches!(self.peek(), Some(':')) {
+            self.advance();
+        }
+
+        self.delete_buffer();
+    }
+
+    fn message(&mut self) {
+        self.collect_text();
+
+        while matches!(self.peek(), Some(':')) {
+            self.advance();
+        }
+
+        self.expect_string(MESSAGE_TAG);
+
+        self.advance_spaces();
+
+        let message_type = if self.matches_keyword("info") {
+            MessageType::Info
+        } else if self.matches_keyword("warn") {
+            MessageType::Warn
+        } else if self.matches_keyword("alert") {
+            MessageType::Alert
+        } else {
+            zeta_error("Invalid message type");
+            MessageType::Info
+        };
+
+        self.extract_until('\n');
+        self.expect_string("\n");
+        self.delete_buffer();
+
+        self.extract_until(':');
+
+        let content = self.consume_buffer().unwrap();
+
+        let parser = Parser::new(content.chars().collect());
+        let content = parser.parse();
+
+        self.result.push(Element::Message {
+            msg_type: message_type,
+            body: content,
+        });
+
+        while matches!(self.peek(), Some(':')) {
+            self.advance();
+        }
+
+        self.delete_buffer();
+    }
+
+    fn code_block(&mut self) {
+        if self.peek_next() != Some('`') {
+            // inline
+            self.expect_string("`");
+            self.extract_until('`');
+            self.expect_string("`");
+        } else if self.matches_keyword("```") {
+            // block
+            self.expect_string("```");
+            self.extract_until('`');
+            self.expect_string("```");
+        }
+    }
+
+    fn image(&mut self) {
+        self.collect_text();
+        self.expect_string("![");
+        self.delete_buffer();
+
+        self.extract_until(']');
+
+        let alt = self.consume_buffer().unwrap_or_default();
+        self.expect_string("](");
+        self.delete_buffer();
+
+        self.extract_until(')');
+
+        let url = self.consume_buffer().unwrap();
+        self.expect_string(")");
+        self.delete_buffer();
+
+        self.result.push(Element::Image { alt, url });
+    }
+
     fn advance(&mut self) -> Option<char> {
         self.position += 1;
         self.source.get(self.position - 1).copied()
-    }
-
-    fn back(&mut self) -> bool {
-        if self.position == 0 {
-            return false;
-        }
-
-        self.position -= 1;
-
-        true
     }
 
     fn peek(&mut self) -> Option<char> {
@@ -338,7 +338,7 @@ impl Parser {
         self.start = self.position;
     }
 
-    fn get_buffer(&mut self) -> Option<String> {
+    fn consume_buffer(&mut self) -> Option<String> {
         if self.start == self.position {
             None
         } else {
@@ -348,9 +348,24 @@ impl Parser {
         }
     }
 
-    fn consume_buffer(&mut self) {
-        if let Some(buffer) = self.get_buffer() {
+    fn expect_string(&mut self, string: &str) -> bool {
+        if self.matches_keyword(string) {
+            self.position += string.len();
+            return true;
+        }
+
+        false
+    }
+
+    fn collect_text(&mut self) {
+        if let Some(buffer) = self.consume_buffer() {
             self.result.push(Element::Text(buffer));
+        }
+    }
+
+    fn extract_until(&mut self, until: char) {
+        while self.peek() != Some(until) && !self.is_at_end() {
+            self.advance();
         }
     }
 
@@ -358,14 +373,5 @@ impl Parser {
         while self.peek() == Some(' ') {
             self.advance();
         }
-    }
-
-    fn consume_string(&mut self, string: &str) -> bool {
-        if self.matches_keyword(string) {
-            self.position += string.len();
-            return true;
-        }
-
-        false
     }
 }
